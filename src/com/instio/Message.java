eb3a41a91aa3be9aebdd9f48f29e4caf8f35c9c0
BennettIronYard <bennettiy12@gmail.com>
2015-10-28 11:24:02 -0400

    initial push

---
+++ src/com/instio/Message.java
@@ -0,0 +1,20 @@
+package com.instio;
+
+/**
+ * Created by BennettIronYard on 10/28/15.
+ */
+public class Message {
+    int id;
+    int replyId;
+    String username;
+    String text;
+
+    public Message(int id, int replyId, String username, String text) {
+        this.id = id;
+        this.replyId = replyId;
+        this.username = username;
+        this.text = text;
+    }
+
+
+}
---
+++ src/com/instio/User.java
@@ -0,0 +1,8 @@
+package com.instio;
+
+/**
+ * Created by BennettIronYard on 10/28/15.
+ */
+public class User {
+    String password;
+}
